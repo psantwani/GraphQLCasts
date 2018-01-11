be429fda82b01d6bb88dc5fb3fb01ef36d5ad766
Piyush Santwani <psantwani@gmail.com>
2018-01-11 17:34:22 +0530

    Update App.js

--- Lyrical-GraphQL/client/components/App.js
+++ Lyrical-GraphQL/client/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//SongList will passed to the App Component as chidlren and we will show that component as a child.
 export default ({ children }) => {
   return <div className="container">{children}</div>;
 };
